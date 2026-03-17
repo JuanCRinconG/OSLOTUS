b46fb605bf7880263783c5a4690e4391458f5ee6
JuanCRinconG <jcrinconguerrero@ucundinamarca.edu.co>
2026-03-17 16:27:53 -0500

    Cambio a clase principal

--- ClasePrincipalLotus.py
+++ ClasePrincipalLotus.py
@@ -50,13 +50,3 @@ class Lotus(QMainWindow):
             self.showNormal()
         else:
             self.showFullScreen()
-
-     
-#A diferencia de tkinter, el motor de PyQt5 se ejecuta en un loop separado de la clase principal.
-#por lo que se debe crear una instancia de QApplication para que sea el motor de las ventanas que creemos 
-#y asi registrar cosas como inputs de mouse o teclado,
-#y ejecutar el loop para que la aplicación funcione correctamente.
-MotorQT = QApplication(sys.argv)
-Lotus = Lotus()
-Lotus.show()
-sys.exit(MotorQT.exec_())
